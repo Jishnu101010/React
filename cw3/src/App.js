c19ea5ce63894969751fb417778c6cdfdd4c567f
Jishnu Shaji KK <jishnushajikk@gmail.com>
2026-02-04 15:18:16 +0530

    React folder with hw3, cw3, practice

---
+++ cw3/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from './Header';
+function App() {
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Hello, ${name}!`);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <h1>Simple React Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ cw3/src/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return (
+        <div>
+            <h1>Welcome to the world of React!</h1>
+        </div>
+    );
+}
+export default Header;
